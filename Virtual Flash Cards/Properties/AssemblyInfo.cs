d4ca2bd3c96ef6e25d5e22abdb944b30763ee3fa
Tim Baxendale <pcluddite@outlook.com>
2021-05-24 21:19:20 -0500

    Add license headers to files

--- Virtual Flash Cards/Properties/AssemblyInfo.cs
+++ Virtual Flash Cards/Properties/AssemblyInfo.cs
@@ -1,4 +1,21 @@
-﻿using System.Reflection;
+﻿//
+//    Virtual Flash Cards
+//    Copyright (C) 2009-2021 Timothy Baxendale
+//
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -10,7 +27,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Timothy Baxendale")]
 [assembly: AssemblyProduct("Virtual Flash Cards")]
-[assembly: AssemblyCopyright("Copyright © Timothy Baxendale 2009-2015")]
+[assembly: AssemblyCopyright("Copyright © Timothy Baxendale 2009-2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +49,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.0.0.0")]
-[assembly: AssemblyFileVersion("3.0.0.0")]
+[assembly: AssemblyVersion("4.0.0.0")]
+[assembly: AssemblyFileVersion("4.0.0.0")]
