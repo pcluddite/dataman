e6404e70b892f1a232cfa9f5f08ef175a4b5faf8
Tim Baxendale <pcluddite@outlook.com>
2021-07-05 17:12:56 -0500

    Add ReadOnlySet

---
+++ DataMan/Collections/ReadOnly/ReadOnlySet.cs
@@ -0,0 +1,94 @@
+﻿//
+//    DataMan - Supplemental library for managing data types and handling serialization
+//    Copyright (C) 2021 Timothy Baxendale
+//
+//    This library is free software; you can redistribute it and/or
+//    modify it under the terms of the GNU Lesser General Public
+//    License as published by the Free Software Foundation; either
+//    version 2.1 of the License, or (at your option) any later version.
+//
+//    This library is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//    Lesser General Public License for more details.
+//
+//    You should have received a copy of the GNU Lesser General Public
+//    License along with this library; if not, write to the Free Software
+//    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+//    USA
+//
+using System;
+using System.Collections.Generic;
+
+namespace Baxendale.DataManagement.Collections.ReadOnly
+{
+    public sealed class ReadOnlySet<T> : ReadOnlyCollectionBase<T>, ISet<T>
+    {
+        private readonly ISet<T> _set;
+
+        public ReadOnlySet(ISet<T> set)
+            : base(set)
+        {
+            _set = set;
+        }
+
+        public bool IsProperSubsetOf(IEnumerable<T> other)
+        {
+            return _set.IsProperSubsetOf(other);
+        }
+
+        public bool IsProperSupersetOf(IEnumerable<T> other)
+        {
+            return _set.IsProperSupersetOf(other);
+        }
+
+        public bool IsSubsetOf(IEnumerable<T> other)
+        {
+            return _set.IsSubsetOf(other);
+        }
+
+        public bool IsSupersetOf(IEnumerable<T> other)
+        {
+            return _set.IsSupersetOf(other);
+        }
+
+        public bool Overlaps(IEnumerable<T> other)
+        {
+            return _set.Overlaps(other);
+        }
+
+        public bool SetEquals(IEnumerable<T> other)
+        {
+            return _set.SetEquals(other);
+        }
+
+        #region ISet<T>
+
+        bool ISet<T>.Add(T item)
+        {
+            throw new NotSupportedException();
+        }
+
+        void ISet<T>.ExceptWith(IEnumerable<T> other)
+        {
+            throw new NotSupportedException();
+        }
+
+        void ISet<T>.IntersectWith(IEnumerable<T> other)
+        {
+            throw new NotSupportedException();
+        }
+
+        void ISet<T>.SymmetricExceptWith(IEnumerable<T> other)
+        {
+            throw new NotSupportedException();
+        }
+
+        void ISet<T>.UnionWith(IEnumerable<T> other)
+        {
+            throw new NotSupportedException();
+        }
+
+        #endregion
+    }
+}
