9ef7c4be9b97c4feef96c626e50db868fe92654b
Tim Baxendale <pcluddite@outlook.com>
2021-07-05 17:34:38 -0500

    Collections<T>.SingletonCollection -> SingletonSet

---
+++ DataMan/Collections/SingletonSet.cs
@@ -0,0 +1,160 @@
+﻿//
+//    DataMan - Supplemental library for managing data types and handling serialization
+//    Copyright (C) 2021 Timothy Baxendale
+//
+//    This library is free software; you can redistribute it and/or
+//    modify it under the terms of the GNU Lesser General Public
+//    License as published by the Free Software Foundation; either
+//    version 2.1 of the License, or (at your option) any later version.
+//
+//    This library is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//    Lesser General Public License for more details.
+//
+//    You should have received a copy of the GNU Lesser General Public
+//    License along with this library; if not, write to the Free Software
+//    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+//    USA
+//
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Baxendale.DataManagement.Collections
+{
+    public class SingletonSet<T> : ISet<T>
+    {
+        private readonly T _value;
+        public IEqualityComparer<T> Comparer { get; }
+
+        public SingletonSet(T value)
+            : this(value, null)
+        {
+        }
+
+        public SingletonSet(T value, IEqualityComparer<T> comparer)
+        {
+            _value = value;
+            Comparer = comparer ?? EqualityComparer<T>.Default;
+        }
+
+        public bool Contains(T item)
+        {
+            return Comparer.Equals(item, _value);
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            if (array == null) throw new ArgumentNullException(nameof(array));
+            if ((uint)arrayIndex >= array.Length) throw new ArgumentOutOfRangeException(nameof(arrayIndex));
+            array[arrayIndex] = _value;
+        }
+
+        public bool IsSubsetOf(IEnumerable<T> other)
+        {
+            if (other == null) throw new ArgumentNullException(nameof(other));
+            return SetEquals(other);
+        }
+
+        public bool IsSupersetOf(IEnumerable<T> other)
+        {
+            if (other == null) throw new ArgumentNullException(nameof(other));
+            return SetEquals(other);
+        }
+
+        public bool IsProperSupersetOf(IEnumerable<T> other)
+        {
+            if (other == null) throw new ArgumentNullException(nameof(other));
+            return false;
+        }
+
+        public bool IsProperSubsetOf(IEnumerable<T> other)
+        {
+            if (other == null) throw new ArgumentNullException(nameof(other));
+            return false;
+        }
+
+        public bool Overlaps(IEnumerable<T> other)
+        {
+            if (other == null) throw new ArgumentNullException(nameof(other));
+            return other.Contains(_value);
+        }
+
+        public bool SetEquals(IEnumerable<T> other)
+        {
+            if (other == null) throw new ArgumentNullException(nameof(other));
+            int i = 0;
+            foreach (T item in other)
+            {
+                if (i > 0 || !Comparer.Equals(_value, item))
+                    return false;
+                ++i;
+            }
+            return true;
+        }
+
+        #region ISet<T>
+
+        bool ISet<T>.Add(T item)
+        {
+            throw new NotSupportedException();
+        }
+
+        void ISet<T>.UnionWith(IEnumerable<T> other)
+        {
+            throw new NotSupportedException();
+        }
+
+        void ISet<T>.IntersectWith(IEnumerable<T> other)
+        {
+            throw new NotSupportedException();
+        }
+
+        void ISet<T>.ExceptWith(IEnumerable<T> other)
+        {
+            throw new NotSupportedException();
+        }
+
+        void ISet<T>.SymmetricExceptWith(IEnumerable<T> other)
+        {
+            throw new NotSupportedException();
+        }
+
+        #endregion
+
+        #region ICollection<T>
+
+        int ICollection<T>.Count => 1;
+        bool ICollection<T>.IsReadOnly => true;
+
+        void ICollection<T>.Add(T item)
+        {
+            throw new NotSupportedException();
+        }
+
+        void ICollection<T>.Clear()
+        {
+            throw new NotSupportedException();
+        }
+
+        bool ICollection<T>.Remove(T item)
+        {
+            throw new NotSupportedException();
+        }
+
+
+        #endregion
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            yield return _value;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
